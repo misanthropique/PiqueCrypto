e5adff04e771a394c1bf5b3e3f57974d29a549dc
misanthropique <80142735+misanthropique@users.noreply.github.com>
2021-03-07 10:18:45 -0600

    Completed wrting unit tests.

--- test/Test_Key.hpp
+++ test/Test_Key.hpp
@@ -232,76 +232,279 @@ TEST( TestKey, CopyAssignmentOperatorShallSetNonNullKeyToNonNullIfOtherIsANonNul
 
 TEST( TestKey, MoveAssignmentShallSetNonNullKeyToNullIfOtherIsANullKeyAndOtherShallBeANullKeyAfterward )
 {
+	static const uint8_t[] nonNullValue = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	Key nullKey;
+	Key nonNullKey( nonNullValue, nonNullValueLength );
+
+	ASSERT_NE( nullptr, nonNullKey.mKeyBuffer );
+	ASSERT_NE( 0, nonNullKey.mKeyLength );
+
+	nonNullKey = std::move( nullKey );
+
+	ASSERT_EQ( nullptr, nonNullKey.mKeyBuffer );
+	ASSERT_EQ( 0, nonNullKey.mKeyLength );
+	ASSERT_EQ( nullptr, nullKey.mKeyBuffer );
+	ASSERT_EQ( 0, nullKey.mKeyLength );
 }
 
 TEST( TestKey, MoveAssignmentShallSetNullKeyToNonNullIfOtherIsANonNullKeyAndOtherShallBeANullKeyAfterward )
 {
+	static const uint8_t[] nonNullValue = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	Key nullKey;
+	Key nonNullKey( nonNullValue, nonNullValueLength );
+
+	ASSERT_EQ( nullptr, nullKey.mKeyBuffer );
+	ASSERT_EQ( 0, nullKey.mKeyLength );
+	ASSERT_NE( nullptr, nonNullKey.mKeyBuffer );
+	ASSERT_NE( 0, nonNullKey.mKeyLength );
+
+	nullKey = std::move( nonNullKey );
+
+	ASSERT_NE( nullptr, nullKey.mKeyBuffer );
+	ASSERT_NE( 0, nullKey.mKeyLength );
+	ASSERT_EQ( nonNullValueLength, nullKey.mKeyLength );
+	ASSERT_EQ( 0, std::memcmp( nonNullValue, nullKey.mKeyBuffer.get(), nonNullValueLength ) );
+
+	ASSERT_EQ( nullptr, nonNullKey.mKeyBuffer );
+	ASSERT_EQ( 0, nonNullKey.mKeyLength );
 }
 
 TEST( TestKey, MoveAssignmentShallSetNonNullKeyToNonNullIfOtherIsANonNullKeyAndOtherShallBeANullKeyAfterward )
 {
+	static const uint8_t[] nonNullValueA = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueALength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	static const uint8_t[] nonNullValueB = { 0x03, 0x02, 0x01, 0x00 };
+	static const size_t nonNullValueBLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	Key nonNullKeyA( nonNullValueA, nonNullValueALength );
+	Key nonNullKeyB( nonNullValueB, nonNullValueBLength );
+
+	ASSERT_NE( nullptr, nonNullKeyA.mKeyBuffer );
+	ASSERT_NE( 0, nonNullKeyA.mKeyLength );
+	ASSERT_NE( nullptr, nonNullKeyB.mKeyBuffer );
+	ASSERT_NE( 0, nonNullKeyB.mKeyLength );
+
+	nonNullKeyA = std::move( nonNullKeyB );
+
+	ASSERT_NE( nullptr, nonNullKeyA.mKeyBuffer );
+	ASSERT_NE( 0, nonNullKeyA.mKeyLength );
+	ASSERT_EQ( nonNullValueBLength, nonNullKeyA.mKeyLength );
+	ASSERT_EQ( 0, std::memcmp( nonNullValueB, nonNullKeyA.mKeyBuffer.get(), nonNullValueBLength ) );
+
+	ASSERT_EQ( nullptr, nonNullKeyB.mKeyBuffer );
+	ASSERT_EQ( 0, nonNullKeyB.mKeyLength );
 }
 
 TEST( TestKey, EqualityOperatorShallReturnFalseIfKeyLengthsAreNotEqual )
 {
+	static const uint8_t[] nonNullValue = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	Key nonNullKeyA( nonNullValue, nonNullValueLength );
+	Key nonNullKeyB( nonNullValue, nonNullValueLength );
+
+	nonNullKeyB.mKeyLength = nonNullKeyA.mKeyLength + 1;
+
+	ASSERT_FALSE( nonNullKeyA == nonNullKeyB );
 }
 
 TEST( TestKey, EqualityOperatorShallReturnTrueIfBothKeysAreNull )
 {
+	Key nullKeyA;
+	Key nullKeyB;
+
+	ASSERT_TRUE( nullKeyA, nullKeyB );
 }
 
 TEST( TestKey, EqualityOperatorShallReturnFalseIfBothKeysAreOfEqualLengthAndDifferingContent )
 {
+	static const uint8_t[] nonNullValueA = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueALength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	static const uint8_t[] nonNullValueB = { 0x00, 0x01, 0x02, 0x03, 0x07, 0x06, 0x05, 0x04 };
+	static const size_t nonNullValueBLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	ASSERT_EQ( nonNullValueALength, nonNullValueBLength );
+	ASSERT_NE( 0, std::memcmp( nonNullValueA, nonNullValueB, nonNullValueALength ) );
+
+	Key nonNullKeyA( nonNullValueA, nonNullValueALength );
+	Key nonNullKeyB( nonNullValueB, nonNullValueBLength );
+
+	ASSERT_EQ( nonNullKeyA.mKeyLength, nonNullKeyB.mKeyLength );
+	ASSERT_FALSE( nonNullKeyA == nonNullKeyB );
 }
 
 TEST( TestKey, EqualityOperatorShallReturnTrueIfBothKeysAreOfEqualLengthAndEqualContent )
 {
+	static const uint8_t[] nonNullValue = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	Key nonNullKeyA( nonNullValue, nonNullValueLength );
+	Key nonNullKeyB( nonNullValue, nonNullValueLength );
+
+	ASSERT_EQ( nonNullKeyA.mKeyLength, nonNullKeyB.mKeyLength );
+	ASSERT_EQ( 0, std::memcmp( nonNullKeyA.mKeyBuffer.get(), nonNullKeyB.mKeyBuffer.get(), nonNullValueLength ) );
+	ASSERT_TRUE( nonNullKeyA == nonNullKeyB );
 }
 
 TEST( TestKey, InequalityOperatorShallReturnTrueIfKeyLengthsAreNotEqual )
 {
+	static const uint8_t[] nonNullValue = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	Key nullKey;
+	Key nonNullKey( nonNullValue, nonNullValueLength );
+
+	ASSERT_NE( nullKey.mKeyLength, nonNullKey.mKeyLength );
+	ASSERT_TRUE( nullKey != nonNullKey );
 }
 
 TEST( TestKey, InequalityOperatorShallReturnFalseIfBothKeysAreNull )
 {
+	Key nullKeyA;
+	Key nullKeyB;
+
+	ASSERT_FALSE( nullKeyA != nullKeyB );
 }
 
 TEST( TestKey, InequalityOperatorShallReturnTrueIfBothKeysAreOfEqualLengthAndDifferingContent )
 {
+	static const uint8_t[] nonNullValueA = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueALength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	static const uint8_t[] nonNullValueB = { 0x00, 0x01, 0x02, 0x03, 0x07, 0x06, 0x05, 0x04 };
+	static const size_t nonNullValueBLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	ASSERT_EQ( nonNullValueALength, nonNullValueBLength );
+	ASSERT_NE( 0, std::memcmp( nonNullValueA, nonNullValueB, nonNullValueALength ) );
+
+	Key nonNullKeyA( nonNullValueA, nonNullValueALength );
+	Key nonNullKeyB( nonNullValueB, nonNullValueBLength );
+
+	ASSERT_EQ( nonNullKeyA.mKeyLength, nonNullKeyB.mKeyLength );
+	ASSERT_NE( 0, std::memcmp( nonNullKeyA.mKeyBuffer.get(), nonNullKeyB.mKeyBuffer.get(), nonNullKeyA.mKeyLength ) );
+	ASSERT_TRUE( nonNullKeyA != nonNullKeyB );
 }
 
 TEST( TestKey, InequalityOperatorShallReturnFalseIfBothKeysAreOfEqualLengthAndEqualContent )
 {
+	static const uint8_t[] nonNullValue = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	Key nonNullKeyA( nonNullValue, nonNullValueLength );
+	Key nonNullKeyB( nonNullValue, nonNullValueLength );
+
+	ASSERT_EQ( nonNullKeyA.mKeyLength, nonNullKeyB.mKeyLength );
+	ASSERT_EQ( 0, std::memcmp( nonNullKeyA.mKeyBuffer.get(), nonNullKeyB.mKeyBuffer.get(), nonNullKeyA.mKeyLength ) );
+	ASSERT_FALSE( nonNullKeyA != nonNullKeyB );
 }
 
 TEST( TestKey, BoolOperatorShallReturnFalseIfKeyIsNull )
 {
+	Key nullKey;
+
+	ASSERT_FALSE( bool( nullKey ) );
 }
 
 TEST( TestKey, BoolOperatorShallReturnTrueIfKeyIsNull )
 {
+	static const uint8_t[] nonNullValue = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	Key nonNullKey( nonNullValue, nonNullValueLength );
+
+	ASSERT_TRUE( bool( nonNullKey ) );
 }
 
 TEST( TestKey, StringOperatorShallReturnAnEmptyStringForANullKey )
 {
+	Key nullKey;
+
+	std::string keyString( nullKey );
+
+	ASSERT_TRUE( keyString == "" );
 }
 
 TEST( TestKey, StringOperatorShallReturnTheHexadecimalRepresentationOfTheKeyContentIfKeyIsNonNull )
 {
+	static const uint8_t[] nonNullValue = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+	static const std::string nonNullValueString( "0001020304050607" );
+
+	Key nonNullKey( nonNullValue, nonNullValueLength );
+	std::string keyString( nonNullKey );
+
+	ASSERT_EQ( nonNullValueLength * 2, keyString.size() );
+	ASSERT_TRUE( keyString == nonNullValueString );
 }
 
 TEST( TestKey, SetShallSetTheKeyToNullIfValueIsNullAndLengthIsZero )
 {
+	static const uint8_t[] nonNullValue = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	Key nonNullKey( nonNullValue, nonNullValueLength );
+
+	ASSERT_NE( nullptr, nonNullKey.mKeyBuffer );
+	ASSERT_NE( 0, nonNullKey.mKeyLength );
+
+	nonNullKey.set( nullptr, 0 );
+
+	ASSERT_EQ( nullptr, nonNullKey.mKeyBuffer );
+	ASSERT_EQ( 0, nonNullKey.mKeyLength );
 }
 
 TEST( TestKey, SetShallSetTheKeyToNullIfValueIsNullAndLengthIsNonZero )
 {
+	static const uint8_t[] nonNullValue = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	Key nonNullKey( nonNullValue, nonNullValueLength );
+
+	ASSERT_NE( nullptr, nonNullKey.mKeyBuffer );
+	ASSERT_NE( 0, nonNullKey.mKeyLength );
+
+	nonNullKey.set( nullptr, 128 );
+
+	ASSERT_EQ( nullptr, nonNullKey.mKeyBuffer );
+	ASSERT_EQ( 0, nonNullKey.mKeyLength );
 }
 
 TEST( TestKey, SetShallSetTheKeyToNullIfLengthIsZeroAndValueIsNonNull )
 {
+	static const uint8_t[] nonNullValue = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	Key nonNullKey( nonNullValue, nonNullValueLength );
+
+	ASSERT_NE( nullptr, nonNullKey.mKeyBuffer );
+	ASSERT_NE( 0, nonNullKey.mKeyLength );
+
+	nonNullKey.set( nonNullValue, 0 );
+
+	ASSERT_EQ( nullptr, nonNullKey.mKeyBuffer );
+	ASSERT_EQ( 0, nonNullKey.mKeyLength );
 }
 
 TEST( TestKey, SetShallSetTheKeyToNonNullIfValueIsNonNullAndLengthIsNonZero )
 {
+	static const uint8_t[] nonNullValue = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+	static const size_t nonNullValueLength = sizeof( nonNullValue ) / sizeof( *nonNullValue );
+
+	Key nullKey;
+
+	ASSERT_EQ( nullptr, nullKey.mKeyBuffer );
+	ASSERT_EQ( 0, nullKey.mKeyLength );
+
+	nullKey.set( nonNullValue, nonNullValueLength );
+
+	ASSERT_NE( nullptr, nullKey.mKeyBuffer );
+	ASSERT_NE( 0, nullKey.mKeyLength );
+	ASSERT_EQ( nullKey.mKeyLength, nonNullValueLength );
+	ASSERT_EQ( 0, std::memcmp( nullKey.mKeyBuffer.get(), nonNullValue, nonNullValueLength ) );
 }
